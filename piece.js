5b6bf3ac0d23d7b79f023de3d30355985c3e583e
taikarir <69635722+taikarir@users.noreply.github.com>
2020-11-25 11:24:03 -0500

    Added possible moves for all pieces

--- piece.js
+++ piece.js
@@ -8,30 +8,15 @@ class Piece {
     possiblemoves() {
         this.possmoves=[];
         if (this.type=="k") {
-            if (this.py>0) {
-                this.possmoves.push([this.px,this.py-1]);
-                if (this.px>0) {
-                    this.possmoves.push([this.px-1,this.py-1]);
-                }   
-                if (this.px<7) {
-                    this.possmoves.push([this.px+1,this.py-1]);
-                }   
-            }   
-            if (this.py<7) {
-                this.possmoves.push([this.px,this.py+1]);
-                if (this.px>0) {
-                    this.possmoves.push([this.px-1,this.py+1]);
-                }   
-                if (this.px<7) {
-                    this.possmoves.push([this.px+1,this.py+1]);
-                }   
-            }   
-            if (this.px<7) {
-                this.possmoves.push([this.px+1,this.py]);
-            }   
-            if (this.px>0) {
-                this.possmoves.push([this.px-1,this.py]);
-            }   
+            for (var i=0;i<8;i++) {
+                for (var j=0;j<8;j++) {
+                    if (abs(i-this.px)<=1 && abs(j-this.py)<=1) {
+                        if (j==this.py && i==this.px) {} else {
+                            this.possmoves.push([i,j]);
+                        }
+                    }
+                }
+            }
         } else if (this.type=="r") {
             for (var i=0;i<8;i++) {
                 if (i!=this.px) {
@@ -41,6 +26,66 @@ class Piece {
                     this.possmoves.push([this.px,i]);
                 }   
             }   
+        } else if (this.type=="b") {
+            for (var i=0;i<8;i++) {
+                for (var j=0;j<8;j++) {
+                    if ((i-this.px)==(j-this.py) || (i-this.px)==(this.py-j)) {
+                        this.possmoves.push([i,j]);
+                    }
+                }
+            }
+        } else if (this.type=="q") {
+            for (var i=0;i<8;i++) {
+                if (i!=this.px) {
+                    this.possmoves.push([i,this.py]);
+                }   
+                if (i!=this.py) {
+                    this.possmoves.push([this.px,i]);
+                }   
+            }   
+            for (var i=0;i<8;i++) {
+                for (var j=0;j<8;j++) {
+                    if ((i-this.px)==(j-this.py) || (i-this.px)==(this.py-j)) {
+                        this.possmoves.push([i,j]);
+                    }
+                }
+            }
+        } else if (this.type=="n") {
+            for (var i=0;i<8;i++) {
+                for (var j=0;j<8;j++) {
+                    if ((abs(i-this.px)==2 && abs(j-this.py)==1) || (abs(i-this.px)==1 && abs(j-this.py)==2)) {
+                        this.possmoves.push([i,j]);
+                    }
+                }
+            }
+        } else if (this.type=="p") {
+            if (this.color=="w") {
+                if (gamestate[this.py-1][this.px]=="  ") {
+                    this.possmoves.push([this.px,this.py-1]);
+                    if (gamestate[this.py-2][this.px]=="  " && this.py==6) {
+                        this.possmoves.push([this.px,this.py-2]);
+                    }
+                }
+                if (pieces[this.py-1][this.px-1].color=="b") {
+                    this.possmoves.push([this.px-1,this.py-1]);
+                }
+                if (pieces[this.py-1][this.px+1].color=="b") {
+                    this.possmoves.push([this.px+1,this.py-1]);
+                }
+            } else if (this.color=="b") {
+                if (gamestate[this.py+1][this.px]=="  ") {
+                    this.possmoves.push([this.px,this.py+1]);
+                    if (gamestate[this.py+2][this.px]=="  " && this.py==1) {
+                        this.possmoves.push([this.px,this.py+2]);
+                    }
+                }
+                if (pieces[this.py+1][this.px-1].color=="w") {
+                    this.possmoves.push([this.px-1,this.py+1]);
+                }
+                if (pieces[this.py+1][this.px+1].color=="w") {
+                    this.possmoves.push([this.px+1,this.py+1]);
+                }
+            }
         }
         return this.possmoves;
     }
